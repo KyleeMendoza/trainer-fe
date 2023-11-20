6f3e667dfa062f9ddb39d3d89d118f3c6bba7c2d
Kyle Dominic Mendoza <kylemendoza67@gmail.com>
2023-11-20 14:37:17 +0800

    save button on table done

--- src/components/Attendance.tsx
+++ src/components/Attendance.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
-import { DataGrid, GridToolbar } from "@mui/x-data-grid";
+import { DataGrid, GridToolbar, GridRowSelectionModel, GridRowId  } from "@mui/x-data-grid";
 import { useDemoData } from "@mui/x-data-grid-generator";
-import Switch from "@mui/material/Switch";
+import MenuItem from '@mui/material/MenuItem';
 
 import {
   GridToolbarColumnsButton,
@@ -11,11 +11,10 @@ import {
   GridToolbarExport,
 } from "@mui/x-data-grid/components";
 
-const label = { inputProps: { "aria-label": "Switch demo" } };
-
-const CustomSwitch = ({ defaultChecked }) => (
-  <Switch {...label} defaultChecked={defaultChecked} />
-);
+import { GridToolbarExportContainer } from "@mui/x-data-grid/components";
+import { GridCsvExportMenuItem } from "@mui/x-data-grid/components";
+import { gridFilteredSortedRowIdsSelector, gridVisibleColumnFieldsSelector, useGridApiContext } from "@mui/x-data-grid";
+import Button from "@mui/material/Button";
 
 const columns = [
   {
@@ -31,14 +30,14 @@ const columns = [
     headerClassName: "bg-md-blue text-white border-r-2 border-black",
   },
   {
-    field: "participantId",
-    headerName: "Participant ID",
+    field: "email",
+    headerName: "Email",
     width: 200,
     headerClassName: "bg-md-blue text-white border-r-2 border-black",
   },
   {
-    field: "arrivalTime",
-    headerName: "Arrival Time",
+    field: "phoneNumber",
+    headerName: "Phone Number",
     width: 200,
     headerClassName: "bg-md-blue text-white border-r-2 border-black",
   },
@@ -48,99 +47,138 @@ const columns = [
     width: 200,
     headerClassName: "bg-md-blue text-white border-r-2 border-black",
   },
-  {
-    field: "action",
-    headerName: "Action",
-    width: 200,
-    headerClassName: "bg-md-blue text-white border-r-2 border-black",
-    renderCell: (params) => (
-      <CustomSwitch {...label} defaultChecked={params.row.action} />
-    ),
-  },
 ];
 
 const data = [
   {
     id: 1,
-    name: "Sophia Miller",
-    participantId: "435467",
-    arrivalTime: "7:30AM (GMT +8)",
-    status: "Present",
-    action: true,
+    name: "John Doe",
+    email: "john@example.com",
+    phoneNumber: "123-456-7890",
+    status: "Absent"
   },
   {
     id: 2,
-    name: "Ethan Johnson",
-    participantId: "987654",
-    arrivalTime: "8:15AM (GMT +8)",
-    status: "Absent",
-    action: true,
+    name: "Jane Smith",
+    email: "jane@example.com",
+    phoneNumber: "987-654-3210",
+    status: "Absent"
   },
   {
     id: 3,
-    name: "Olivia Davis",
-    participantId: "123456",
-    arrivalTime: "7:55AM (GMT +8)",
-    status: "Present",
-    action: true,
+    name: "Alice Johnson",
+    email: "alice@example.com",
+    phoneNumber: "555-123-4567",
+    status: "Absent"
   },
   {
     id: 4,
-    name: "Noah Smith",
-    participantId: "567890",
-    arrivalTime: "7:40AM (GMT +8)",
-    status: "Present",
-    action: true,
+    name: "Bob Anderson",
+    email: "bob@example.com",
+    phoneNumber: "777-888-9999",
+    status: "Absent"
   },
   {
     id: 5,
-    name: "Ava Wilson",
-    participantId: "234567",
-    arrivalTime: "8:00AM (GMT +8)",
-    status: "Absent",
-    action: true,
+    name: "Eva Williams",
+    email: "eva@example.com",
+    phoneNumber: "111-222-3333",
+    status: "Absent"
   },
   {
     id: 6,
-    name: "Liam Brown",
-    participantId: "876543",
-    arrivalTime: "7:25AM (GMT +8)",
-    status: "Present",
-    action: true,
+    name: "Michael Brown",
+    email: "michael@example.com",
+    phoneNumber: "444-555-6666",
+    status: "Absent"
   },
   {
     id: 7,
-    name: "Emma Lee",
-    participantId: "345678",
-    arrivalTime: "7:45AM (GMT +8)",
-    status: "Absent",
-    action: true,
+    name: "Sophia Davis",
+    email: "sophia@example.com",
+    phoneNumber: "666-777-8888",
+    status: "Absent"
   },
   {
     id: 8,
-    name: "Jackson Wang",
-    participantId: "654321",
-    arrivalTime: "8:10AM (GMT +8)",
-    status: "Present",
-    action: true,
+    name: "Matthew Miller",
+    email: "matthew@example.com",
+    phoneNumber: "888-999-0000",
+    status: "Absent"
   },
   {
     id: 9,
-    name: "Isabella Chen",
-    participantId: "789012",
-    arrivalTime: "8:20AM (GMT +8)",
-    status: "Present",
-    action: true,
+    name: "Olivia Wilson",
+    email: "olivia@example.com",
+    phoneNumber: "222-333-4444",
+    status: "Absent"
   },
   {
     id: 10,
-    name: "Lucas Kim",
-    participantId: "210987",
-    arrivalTime: "7:35AM (GMT +8)",
-    status: "Absent",
-    action: true,
+    name: "Daniel Lee",
+    email: "daniel@example.com",
+    phoneNumber: "333-444-5555",
+    status: "Absent"
   },
 ];
+
+//MAKING IT JSON
+// const getJson = (apiRef) => {
+//   // Select rows and columns
+//   const filteredSortedRowIds = gridFilteredSortedRowIdsSelector(apiRef);
+//   const visibleColumnsField = gridVisibleColumnFieldsSelector(apiRef);
+
+//   // Format the data. Here we only keep the value
+//   const data = filteredSortedRowIds.map((id) => {
+//     const row = {};
+//     visibleColumnsField.forEach((field) => {
+//       row[field] = apiRef.current.getCellParams(id, field).value;
+//     });
+//     return row;
+//   });
+
+//   return JSON.stringify(data, null, 2);
+// };
+
+// const exportBlob = (blob, filename) => {
+//   // Save the blob in a json file
+//   const url = URL.createObjectURL(blob);
+
+//   const a = document.createElement('a');
+//   a.href = url;
+//   a.download = filename;
+//   a.click();
+
+//   setTimeout(() => {
+//     URL.revokeObjectURL(url);
+//   });
+// };
+
+// function JsonExportMenuItem(props) {
+//   const apiRef = useGridApiContext();
+
+//   const { hideMenu } = props;
+
+//   return (
+//     <MenuItem
+//       onClick={() => {
+//         const jsonString = getJson(apiRef);
+//         console.log(jsonString)
+//         const blob = new Blob([jsonString], {
+//           type: 'text/json',
+//         });
+//         exportBlob(blob, 'Attendance.json');
+
+//         // Hide the export menu after the export
+//         hideMenu?.();
+//       }}
+//     >
+//       Download as JSON
+//     </MenuItem>
+//   );
+// }
+
+// const csvOptions = { delimiter: ';' };
 
 function CustomToolbar() {
   return (
@@ -149,18 +187,70 @@ function CustomToolbar() {
       <GridToolbarFilterButton />
       <GridToolbarDensitySelector />
       <GridToolbarExport />
+      {/* <CustomExportButton/> */}
     </GridToolbarContainer>
   );
 }
 
+// function CustomExportButton(props) {
+//   return (
+//     <GridToolbarExportContainer {...props}>
+//       <GridCsvExportMenuItem options={csvOptions} />
+//       <JsonExportMenuItem />
+//     </GridToolbarExportContainer>
+//   );
+// }
+
 export default function Attendance() {
+
+  const [rowSelectionModel, setRowSelectionModel] =
+  React.useState<GridRowSelectionModel>([]);
+
+  const [selectedNames, setSelectedNames] = React.useState<string[]>([]);
+
+  const [updatedData, setUpdatedData] = React.useState<object[]>([]);
+
+  // React.useEffect(() => {
+  //   // console.log(selectedNames)
+  //   console.log(updatedData)
+  // }, [updatedData])
+
+  React.useEffect(() => {
+     // Map over the original data and create a new array with updated status
+    const updatedDataArray = data.map((item) => {
+      // Check if the name is in the list to be updated
+      if (selectedNames.includes(item.name)) {
+        return { ...item, status: "Present" };
+      } else {
+        return { ...item };
+      }
+    });
+
+    // Update the state with the new array
+    setUpdatedData(updatedDataArray);
+  }, [data, selectedNames])
+
   return (
     <div style={{ height: 550, width: "100%", padding: "1.5rem" }}>
-      <DataGrid
+       <DataGrid
         rows={data}
         columns={columns}
         slots={{ toolbar: CustomToolbar }}
+        checkboxSelection
+        onRowSelectionModelChange={(newRowSelectionModel) => {
+          setRowSelectionModel(newRowSelectionModel);
+
+          const names = newRowSelectionModel
+            .filter((isSelected, index) => isSelected)
+            .map((index:any) => data[index - 1].name);
+
+          setSelectedNames(names);
+        }}
+        rowSelectionModel={rowSelectionModel}
       />
+      <Button variant="contained" onClick={() => console.log(updatedData)}>
+        Save
+      </Button>
     </div>
   );
 }
